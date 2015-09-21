65b42792a0397894c46f28714b6a96f462b219a7
James Cancilla <cancilla@ca.ibm.com>
2015-09-21 08:23:13 -0400

    Update copyright notices

--- com.ibm.streamsx.network/com.ibm.streamsx.network.enrichment/IPASNEnricher/IPASNEnricherCommon.pm
+++ com.ibm.streamsx.network/com.ibm.streamsx.network.enrichment/IPASNEnricher/IPASNEnricherCommon.pm
@@ -1,4 +1,4 @@
-## Copyright (C) 2011, 2015  International Business Machines Corporation
+## Copyright (C) 2015  International Business Machines Corporation
 ## All Rights Reserved
 
 package IPASNEnricherCommon;
