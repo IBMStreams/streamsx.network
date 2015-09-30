5759f8a737a703ba39adbf5d1ce6a7c7a12be4e9
Edward Pring <pring@us.ibm.com>
2015-09-30 13:25:22 -0400

    initial version of DHCPMessageParser

--- samples/SampleDHCPMessageParser/script/testDHCPMessageParserFull.sh
+++ samples/SampleDHCPMessageParser/script/testDHCPMessageParserFull.sh
@@ -9,33 +9,30 @@
 #set -o pipefail
 
 namespace=sample
-composite=TestPacketLiveSourceBasic1
+composite=TestDHCPMessageParserFull
 
 here=$( cd ${0%/*} ; pwd )
 projectDirectory=$( cd $here/.. ; pwd )
 toolkitDirectory=$( cd $here/../../.. ; pwd )
 
-buildDirectory=$projectDirectory/output/build/$composite.distributed
+buildDirectory=$projectDirectory/output/build/$composite
+
 dataDirectory=$projectDirectory/data
-logDirectory=$projectDirectory/log
 
 libpcapDirectory=$HOME/libpcap-1.7.4
 
 coreCount=$( cat /proc/cpuinfo | grep processor | wc -l )
 
-domain=CapabilitiesDomain
-instance=CapabilitiesInstance
-
 toolkitList=(
 $toolkitDirectory/com.ibm.streamsx.network
+$toolkitDirectory/samples/SampleNetworkToolkitData
 )
 
 compilerOptionsList=(
 --verbose-mode
 --rebuild-toolkits
 --spl-path=$( IFS=: ; echo "${toolkitList[*]}" )
---part-mode=FALL
---allow-convenience-fusion-options
+--standalone-application
 --optimized-code-generation
 --cxx-flags=-g3
 --static-link
@@ -49,11 +46,11 @@ compileTimeParameterList=(
 )
 
 submitParameterList=(
-networkInterface=eno1
-timeoutInterval=30.0
+pcapFilename=$toolkitDirectory/samples/SampleNetworkToolkitData/sample_dns+dhcp.pcap
+#pcapFilename=$HOME/data.yorktown/splanet02_dns+dhcp_one_minute.pcap
 )
 
-tracing=info # ... one of ... off, error, warn, info, debug, trace
+traceLevel=3 # ... 0 for off, 1 for error, 2 for warn, 3 for info, 4 for debug, 5 for trace
 
 ################### functions used in this script #############################
 
@@ -62,8 +59,6 @@ step() { echo ; echo -e "\e[1;34m$*\e[0m" ; }
 
 ################################################################################
 
-[ -d $logDirectory ] || mkdir -p $logDirectory || echo "sorry, could not create directory '$logDirectory', $?"
-
 cd $projectDirectory || die "Sorry, could not change to $projectDirectory, $?"
 
 #[ ! -d $buildDirectory ] || rm -rf $buildDirectory || die "Sorry, could not delete old '$buildDirectory', $?"
@@ -71,36 +66,19 @@ cd $projectDirectory || die "Sorry, could not change to $projectDirectory, $?"
 [ -d $libpcapDirectory ] && export STREAMS_ADAPTERS_LIBPCAP_INCLUDEPATH=$libpcapDirectory
 [ -d $libpcapDirectory ] && export STREAMS_ADAPTERS_LIBPCAP_LIBPATH=$libpcapDirectory
 
-step "configuration for distributed application '$namespace.$composite' ..."
+step "configuration for standalone application '$namespace.$composite' ..."
 ( IFS=$'\n' ; echo -e "\nStreams toolkits:\n${toolkitList[*]}" )
 ( IFS=$'\n' ; echo -e "\nStreams compiler options:\n${compilerOptionsList[*]}" )
 ( IFS=$'\n' ; echo -e "\n$composite compile-time parameters:\n${compileTimeParameterList[*]}" )
 ( IFS=$'\n' ; echo -e "\n$composite submission-time parameters:\n${submitParameterList[*]}" )
-echo -e "\ndomain: $domain"
-echo -e "\ninstance: $instance"
-echo -e "\ntracing: $tracing"
+echo -e "\ntrace level: $traceLevel"
 
-step "building distributed application '$namespace.$composite' ..."
+step "building standalone application '$namespace.$composite' ..."
 sc ${compilerOptionsList[*]} -- ${compileTimeParameterList[*]} || die "Sorry, could not build '$composite', $?" 
 
-step "granting read permission for instance '$instance' log directory to user '$USER' ..."
-sudo chmod o+r -R /tmp/Streams-$domain/logs/$HOSTNAME/instances
-
-step "submitting distributed application '$namespace.$composite' ..."
-bundle=$buildDirectory/$namespace.$composite.sab
-parameters=$( printf ' --P %s' ${submitParameterList[*]} )
-streamtool submitjob -i $instance -d $domain --config tracing=$tracing $parameters $bundle || die "sorry, could not submit application '$composite', $?"
-
-step "waiting while application runs ..."
-sleep 15
-
-step "getting logs for instance $instance ..."
-streamtool getlog -i $instance -d $domain --includeapps --file $logDirectory/$composite.distributed.logs.tar.gz || die "sorry, could not get logs, $!"
-
-step "cancelling distributed application '$namespace.$composite' ..."
-jobs=$( streamtool lspes -i $instance -d $domain | grep $namespace::$composite | gawk '{ print $1 }' )
-#streamtool canceljob -i $instance -d $domain --collectlogs ${jobs[*]} --trace trace || die "sorry, could not cancel application, $!"
-streamtool canceljob -i $instance -d $domain --collectlogs ${jobs[*]} || die "sorry, could not cancel application, $!"
+step "executing standalone application '$namespace.$composite' ..."
+executable=$buildDirectory/bin/$namespace.$composite
+$executable -t $traceLevel ${submitParameterList[*]} || die "sorry, application '$composite' failed, $?"
 
 exit 0
 
