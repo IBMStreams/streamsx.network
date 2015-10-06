89743a240ffcaaa800bcdcf6474dab77d30cb600
Edward Pring <pring@us.ibm.com>
2015-10-06 10:29:36 -0400

    clarify how to grant Linux capabilities for PacketLiveSource in documentation

--- samples/SamplePacketFileSource/script/setupConsistent.sh
+++ samples/SamplePacketFileSource/script/setupConsistent.sh
@@ -15,11 +15,7 @@ administrator=streamsadmin
 domain=ConsistentDomain
 instance=ConsistentInstance
 
-streamtool=$STREAMS_INSTALL/bin/streamtool
-zkconnect=$( $streamtool getzk --short )
 hostname=$( hostname )
-
-export STREAMS_ZKCONNECT=$zkconnect
 
 ################### functions used in this script #############################
 
@@ -30,33 +26,38 @@ step() { echo ; echo -e "\e[1;34m$*\e[0m" ; }
 
 [ -d $checkpointDirectory ] || mkdir -p $checkpointDirectory || die "sorry, could not create directory '$checkpointDirectory', $!"
 
+step "getting zookeeper connection string ..."
+zkconnect=$( streamtool getzk --short )
+[ -n "$zkconnect" ] || die "sorry, could not get zookeeper connection string"
+export STREAMS_ZKCONNECT=$zkconnect
+
 step "checking zookeeper at '$zkconnect' ..."
-$streamtool getzkstate || die "sorry, could not connect to zookeeper at '$zkconnect', $?"
+streamtool getzkstate || die "sorry, could not connect to zookeeper at '$zkconnect', $?"
 
 $projectDirectory/teardownConsistent.sh
 
 step "making Streams domain '$domain' ..."
-$streamtool mkdomain -d $domain || die "sorry, could not make Streams domain '$domain', $?"
-$streamtool genkey -d $domain || die "sorry, could not generate keys for Streams domain '$domain', $?"
+streamtool mkdomain -d $domain || die "sorry, could not make Streams domain '$domain', $?"
+streamtool genkey -d $domain || die "sorry, could not generate keys for Streams domain '$domain', $?"
 
 step "setting dynamic service ports for Streams domain '$domain' ..."
-$streamtool setdomainproperty -d $domain jmx.port=0 jmx.startTimeout=60 || die "sorry, could not set JMX properties for Streams domain '$domain', $?"
-$streamtool setdomainproperty -d $domain sws.port=0 sws.startTimeout=60 || die "sorry, could not set SWS properties for Streams domain '$domain', $?"
+streamtool setdomainproperty -d $domain jmx.port=0 jmx.startTimeout=60 || die "sorry, could not set JMX properties for Streams domain '$domain', $?"
+streamtool setdomainproperty -d $domain sws.port=0 sws.startTimeout=60 || die "sorry, could not set SWS properties for Streams domain '$domain', $?"
 
 step "starting Streams domain '$domain' ..."
-$streamtool startdomain -d $domain || die "sorry, could not start Streams domain '$domain', $?"
+streamtool startdomain -d $domain || die "sorry, could not start Streams domain '$domain', $?"
 
 step "adding users to Streams domain '$domain' ..."
-$streamtool adduserdomainrole -d $domain DomainAdministrator $administrator || die "sorry, could not add administrator to Streams domain '$domain', $?"
-$streamtool adduserdomainrole -d $domain DomainUser $USER || die "sorry, could not add user to Streams domain '$domain', $?"
-$streamtool lsdomainrole -d $domain
+streamtool adduserdomainrole -d $domain DomainAdministrator $administrator || die "sorry, could not add administrator to Streams domain '$domain', $?"
+streamtool adduserdomainrole -d $domain DomainUser $USER || die "sorry, could not add user to Streams domain '$domain', $?"
+streamtool lsdomainrole -d $domain
 
 step "getting service URLs for Streams domain '$domain' ..."
-$streamtool getjmxconnect -d $domain || die "sorry, could not domain service URL for Streams domain '$domain', $?"
-$streamtool geturl -d $domain || die "sorry, could not get Streams console URL for Streams domain '$domain', $?"
+streamtool getjmxconnect -d $domain || die "sorry, could not domain service URL for Streams domain '$domain', $?"
+streamtool geturl -d $domain || die "sorry, could not get Streams console URL for Streams domain '$domain', $?"
 
 step "making Streams instance '$instance' ..."
-$streamtool mkinstance -i $instance -d $domain \
+streamtool mkinstance -i $instance -d $domain \
 --hosts "$hostname" \
 --property instanceTrace.defaultLevel=info \
 --property instanceTrace.maximumFileCount=10 \
@@ -66,6 +67,6 @@ $streamtool mkinstance -i $instance -d $domain \
 || die "Sorry, could not create Streams instance '$instance', $?"
 
 step "starting Streams instance '$instance' ..."
-$streamtool startinstance -i $instance -d $domain || die "Sorry, could not start Streams instance '$instance', $?" 
+streamtool startinstance -i $instance -d $domain || die "Sorry, could not start Streams instance '$instance', $?" 
 
 exit 0
--- samples/SamplePacketFileSource/script/teardownConsistent.sh
+++ samples/SamplePacketFileSource/script/teardownConsistent.sh
@@ -11,11 +11,6 @@
 domain=ConsistentDomain
 instance=ConsistentInstance
 
-streamtool=$STREAMS_INSTALL/bin/streamtool
-zkconnect=$( $streamtool getzk --short )
-
-export STREAMS_ZKCONNECT=$zkconnect
-
 ################### functions used in this script #############################
 
 die() { echo ; echo -e "\e[1;31m$*\e[0m" >&2 ; exit 1 ; }
@@ -23,10 +18,15 @@ step() { echo ; echo -e "\e[1;34m$*\e[0m" ; }
 
 ################################################################################
 
+#step "getting zookeeper connection string ..."
+zkconnect=$( streamtool getzk --short )
+[ -n "$zkconnect" ] || die "sorry, could not get zookeeper connection string"
+export STREAMS_ZKCONNECT=$zkconnect
+
 step "tearing down previous Streams 'consistent' instance and domain ..."
-$streamtool stopinstance -i $instance -d $domain
-$streamtool rminstance -i $instance -d $domain --noprompt
-$streamtool stopdomain -d $domain
-$streamtool rmdomain -d $domain --noprompt
+streamtool stopinstance -i $instance -d $domain
+streamtool rminstance -i $instance -d $domain --noprompt
+streamtool stopdomain -d $domain
+streamtool rmdomain -d $domain --noprompt
 
 exit 0
