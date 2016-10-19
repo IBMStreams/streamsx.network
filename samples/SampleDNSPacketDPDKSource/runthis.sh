63f3699238678b5b0b8a0cbf72c16ee3292af68b
Edward Pring <pring@us.ibm.com>
2016-10-18 20:41:51 -0400

    more-or-less complete DNSPacketDPDKSource operator and sample applications

--- samples/SampleDNSPacketDPDKSource/runthis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo STREAMS_INSTALL=$STREAMS_INSTALL ./output/bin/standalone -t 3 
--- samples/SampleDNSPacketDPDKSource/script/liveDNSPacketDPDKSourceFull.sh
+++ samples/SampleDNSPacketDPDKSource/script/liveDNSPacketDPDKSourceFull.sh
@@ -21,9 +21,7 @@ unbundleDirectory=$projectDirectory/output/unbundle/$composite
 
 dataDirectory=$projectDirectory/data
 
-libpcapDirectory=$HOME/libpcap-1.7.4
-
-networkInterface=$( ifconfig eno1 1>/dev/null 2>&1 && echo eno1 || echo eth0 ) 
+dpdkDirectory=$RTE_SDK/build/lib
 
 coreCount=$( cat /proc/cpuinfo | grep processor | wc -l )
 
@@ -37,13 +35,16 @@ compilerOptionsList=(
 --spl-path=$( IFS=: ; echo "${toolkitList[*]}" )
 --standalone-application
 --optimized-code-generation
---cxx-flags=-g3
 --static-link
 --main-composite=$namespace::$composite
 --output-directory=$buildDirectory 
 --data-directory=data
 --num-make-threads=$coreCount
 )
+
+gccOptions="--cxx-flags=-g3"
+
+ldOptions="--ld-flags=-Wl,-L -Wl,$dpdkDirectory -Wl,--no-as-needed -Wl,-export-dynamic -Wl,--whole-archive -Wl,-ldpdk -Wl,-libverbs -Wl,-lrt -Wl,-lm -Wl,-ldl -Wl,--no-whole-archive"
 
 compileTimeParameterList=(
 )
@@ -65,31 +66,27 @@ cd $projectDirectory || die "Sorry, could not change to $projectDirectory, $?"
 
 #[ ! -d $buildDirectory ] || rm -rf $buildDirectory || die "Sorry, could not delete old '$buildDirectory', $?"
 [ -d $dataDirectory ] || mkdir -p $dataDirectory || die "Sorry, could not create '$dataDirectory, $?"
-[ -d $libpcapDirectory ] && export STREAMS_ADAPTERS_LIBPCAP_INCLUDEPATH=$libpcapDirectory
-[ -d $libpcapDirectory ] && export STREAMS_ADAPTERS_LIBPCAP_LIBPATH=$libpcapDirectory
 
 step "configuration for standalone application '$namespace.$composite' ..."
 ( IFS=$'\n' ; echo -e "\nStreams toolkits:\n${toolkitList[*]}" )
 ( IFS=$'\n' ; echo -e "\nStreams compiler options:\n${compilerOptionsList[*]}" )
+echo -e "\n$GNU compiler parameters:\n$gccOptions" 
+echo -e "\n$GNU linker parameters:\n$ldOptions" 
 ( IFS=$'\n' ; echo -e "\n$composite compile-time parameters:\n${compileTimeParameterList[*]}" )
 ( IFS=$'\n' ; echo -e "\n$composite submission-time parameters:\n${submitParameterList[*]}" )
 echo -e "\ntrace level: $traceLevel"
 
 step "building standalone application '$namespace.$composite' ..."
-sc ${compilerOptionsList[*]} -- ${compileTimeParameterList[*]} || die "Sorry, could not build '$composite', $?" 
+sc ${compilerOptionsList[*]} "$gccOptions" "$ldOptions" -- "${compileTimeParameterList[*]}" || die "Sorry, could not build '$composite', $?" 
 
-step "unbundling standalone application '$namespace.$composite' ..."
-bundle=$buildDirectory/$namespace.$composite.sab
-[ -f $bundle ] || die "sorry, bundle '$bundle' not found"
-spl-app-info $bundle --unbundle $unbundleDirectory || die "sorry, could not unbundle '$bundle', $?"
-
-step "setting capabilities for standalone application '$namespace.$composite' ..."
-standalone=$unbundleDirectory/$composite/bin/standalone
-[ -f $standalone ] || die "sorry, standalone application '$standalone' not found"
-sudo /usr/sbin/setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' $standalone || die "sorry, could not set capabilities for application '$composite', $?"
-/usr/sbin/getcap -v $standalone || die "sorry, could not get capabilities for application '$composite', $?"
+step "deleting old '/dev/hugepages/rtemap_*' files, if necessary ..."
+[[ ! -f /dev/hugepages/rtemap_* ]] || sudo rm /dev/hugepages/rtemap_* || die "sorry, could not delete /dev/hugepages/rtemap_* files, $?"
 
 step "executing standalone application '$namespace.$composite' ..."
-$standalone -t $traceLevel ${submitParameterList[*]} || die "sorry, application '$composite' failed, $?"
+executable=$buildDirectory/bin/$namespace.$composite
+$executable -t $traceLevel ${submitParameterList[*]}
+
+step "deleting '/dev/hugepages/rtemap_*' files ..."
+[[ ! -f /dev/hugepages/rtemap_* ]] || sudo rm /dev/hugepages/rtemap_* || die "sorry, could not delete /dev/hugepages/rtemap_* files, $?"
 
 exit 0
