06751b66464b1591dc0544aaa30a3a8a844aff96
Edward Pring <pring@us.ibm.com>
2016-10-20 21:42:50 -0400

    add list of DNS parser error codes

---
+++ com.ibm.streamsx.network/impl/include/parse/DNSMessageParserErrorCodes.h
@@ -0,0 +1,23 @@
+
+/*
+
+ 1 contradictory flags
+ 2 label overruns packet
+ 3 label compression length overruns packet
+ 4 label compression offset underruns packet
+ 5 label compression offset loop
+ 6 label compression offset overruns packet
+ 7 label flags invalid
+ 8 label limit exceeded
+ 9 resource record missing
+10 question resource record truncated
+11 resource record truncated
+12 resource record data truncated
+13 too many labels 
+14 label flags invalid
+15 invalid address family
+16 unexpected resource type
+17 message too short
+18 counts too large
+
+*/
